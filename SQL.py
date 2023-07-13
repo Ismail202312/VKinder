61db3ecb947188d0ee0d1af3f23ae751bed8009a
Ismail202312 <137602205+Ismail202312@users.noreply.github.com>
2023-07-13 19:37:38 +0500

    Update SQL.py

--- SQL.py
+++ SQL.py
@@ -3,19 +3,18 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import Table, Column, String, Integer, ForeignKey, MetaData, Select
 
 # Не забываем подставлять свой пароль и имя пользователя
+DSN = 'postgresql://postgres:Markouno123@localhost:5432/VKinder'  # Определяем параметры подключения к базе данных
 DSN = 'postgresql://login:password@localhost:5432/VKinder'  # Определяем параметры подключения к базе данных
 engine = sqlalchemy.create_engine(DSN)  # Создаем движок подключения
 Session = sessionmaker(bind=engine)  # Создаем сессию в которую передаем движок подключения
 session = Session()  # Создаем объект сессии
 metadata = MetaData()  # Создаем объект метаданных
-
 users = Table('users', metadata,  # Создаем таблицу пользователя
               Column('id', Integer, primary_key=True),  # Присваиваем автоматический идентификатор пользователя
               Column('vk_id', String(20), nullable=False),  # Создаем поле vk_user с типом данных String
               Column('gender', String(15), nullable=False),  # String(15) - где 15 ограничение по количеству символов
               Column('age', String(20), nullable=False),
               Column('city', String(83), nullable=False))
-
 pair = Table('pair', metadata,  # Создаем таблицу результата поиска людей по параметрам пользователя
              Column('id', Integer, primary_key=True),
              Column('vk_id', String(20), nullable=False),
@@ -24,19 +23,13 @@ pair = Table('pair', metadata,  # Создаем таблицу результа
              Column('city', String(200), nullable=False),
              Column('profile_url', String(200), nullable=False),
              Column('photos', String(1500), nullable=False))
-
 favorite = Table('favorite', metadata,  # Создаем таблицу для обектов pair добавленных в избранное, для каждого users
                  Column('id', Integer, primary_key=True),
                  Column('users_id', Integer, ForeignKey('users.id')),
                  Column('pair_id', Integer, ForeignKey('pair.id')))
-
-
 def create_table_in_base():  # Функция создания таблиц в базе данных используя объект MetaData
     metadata.create_all(engine)
-
-
 # create_table_in_base()  # Вызов функции создания таблиц в базе данных
-
 def push_user_data_in_base(vk_id, gender, age, city):
     if gender == 'Мужской':  # Фильтр по гендеру
         gender = '2'
@@ -51,8 +44,6 @@ def push_user_data_in_base(vk_id, gender, age, city):
     session.execute(insert_values)  # добавляем записи в базу
     session.commit()  # фиксируем изменения в базе
     session.close()  # закрываем соединение с базой
-
-
 def push_pair_data_in_base(vk_id, first_name, last_name, city, profile_url, photos):
     insert_values = pair.insert().values(  # Определяем колонки и их значения для записи в базу
         vk_id=vk_id,
@@ -65,8 +56,6 @@ def push_pair_data_in_base(vk_id, first_name, last_name, city, profile_url, phot
     session.execute(insert_values)  # добавляем записи в базу
     session.commit()  # фиксируем изменения в базе
     session.close()  # закрываем соединение с базой
-
-
 def push_pair_in_favorite(vk_id, pair_id):  # Добавляем user и pair в избранное, устанавливаем связь
     data = get_user_data(vk_id)  # Получаем данные users
     users_id = data[0]  # записываем id пользователя VK в переменную
@@ -77,8 +66,6 @@ def push_pair_in_favorite(vk_id, pair_id):  # Добавляем user и pair в
     session.execute(insert_values)
     session.commit()
     session.close()
-
-
 def get_user_data(vk_id):  # select запрос в таблицу users
     select_query = Select(users.c.id, users.c.gender, users.c.age, users.c.city
                           ).where(
@@ -87,8 +74,6 @@ def get_user_data(vk_id):  # select запрос в таблицу users
     rows = result.fetchone()  # Вся информация интересующая нас лежит здесь
     session.close()
     return rows
-
-
 def get_pair_data(vk_id):  # select запрос в таблицу pair
     selection_query = Select(pair.c.id, pair.c.first_name, pair.c.last_name, pair.c.profile_url, pair.c.photos
                              ).where(
@@ -97,8 +82,6 @@ def get_pair_data(vk_id):  # select запрос в таблицу pair
     rows = result.fetchall()  # Достаем результаты select запроса и также записываем в переменную
     session.close()
     return rows
-
-
 def get_favorite_data(vk_id):  # select запрос в таблицу favorite
     selection_query = Select(
         pair.c.first_name, pair.c.last_name, pair.c.profile_url, pair.c.photos
