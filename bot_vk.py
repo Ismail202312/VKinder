3af72789ff1216f923f3ab32beec7a7f65b6d098
Ismail202312 <137602205+Ismail202312@users.noreply.github.com>
2023-07-13 19:36:02 +0500

    bot_vk.py

--- bot_vk.py
+++ bot_vk.py
@@ -5,35 +5,25 @@ from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 from tokens import community_token, user_token
 from sql.SQL_scripts import *
 from parser_vk import VK_Parse
-
-
 vk = vk_api.VkApi(token=community_token)
 longpoll = VkLongPoll(vk)
-
 answer_list = ['Привет!', 'Приветствую!', 'Здравствуйте!', 'Здарова!']
 hello_list = ['Привет', 'привет', 'Салам',
               'салам', 'Хай', 'хай', 'Здарова', 'здарова',
               'Начать', 'Start', 'Начать поиск']
-
-
 def write_msg(user_id, message, keyboard=None, attachment=None):
     parametrs = {'user_id': user_id,
                  'message': message,
                  'random_id': random.randrange(10 ** 7),
                  "attachment": attachment
                  }
-
     if keyboard != None:
         parametrs['keyboard'] = keyboard.get_keyboard()
     else:
         parametrs = parametrs
-
     vk.method('messages.send', parametrs)
-
-
 for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW:
-
         if event.to_me:
             request = event.text
             filter_list = event.text.split(', ')
@@ -43,7 +33,6 @@ for event in longpoll.listen():
                     event.user_id, f'{random.choice(answer_list)}')  # Приветствие от бота пользователю
                 write_msg(
                     event.user_id, f'Подскажи, кого мы ищем?&#128527;\nУкажи через запятую пол, возраст и город.\nПример: Мужской, 26, Москва')
-
             # Данное условие ГРУБО даёт понять, что боту переданы параметры для поиска
             elif len(filter_list) == 3:
                 gender = filter_list[0]
@@ -52,6 +41,7 @@ for event in longpoll.listen():
                 write_msg(
                     event.user_id, f'&#128269; Начинаю поиск по параметрам: {gender}, {age}, {city}.\nЭто займет менее минуты...')
 
+                vk_parser = VK_Parse(user_token, event.user_id, gender, age, city)
                 vk_parser = VK_Parse(
                     user_token, event.user_id, gender, age, city)
                 parser_data = vk_parser.parse()  # Начало парсинга совпадений
@@ -60,42 +50,50 @@ for event in longpoll.listen():
                     write_msg(event.user_id, bad_answer)
                 else:
                     # Метод для записи параметров поиска в базу данных users
+                    push_user_data_in_base(event.user_id, gender, age, city)            
+                    chat_button.add_button('Давай посмотрим', VkKeyboardColor.POSITIVE)
                     push_user_data_in_base(event.user_id, gender, age, city)
                     chat_button.add_button(
                         'Давай посмотрим', VkKeyboardColor.POSITIVE)
                     chat_button.add_button('Нет', VkKeyboardColor.NEGATIVE)
                     write_msg(
                         event.user_id, f'{parser_data} Начинаем?', chat_button)
-
-            elif request == 'Давай посмотрим':
+@@ -69,7 +71,8 @@ def write_msg(user_id, message, keyboard=None, attachment=None):
                 chat_button.add_button('Нравится', VkKeyboardColor.POSITIVE)
                 chat_button.add_button('Дальше', VkKeyboardColor.NEGATIVE)
                 chat_button.add_button('Стоп', VkKeyboardColor.SECONDARY)
+                pair_list = get_pair_data(event.user_id)  # Метод, возвращающий совпадения из базы данных
                 # Метод, возвращающий совпадения из базы данных
                 pair_list = get_pair_data(event.user_id)
                 for item in pair_list:
                     # Присваиваем переменные
                     id, first_name, last_name, page, photos = item[0], item[1], item[2], item[3], item[4]
-                    # Убираем срезом фигурные скобки после селекта из базы
-                    photos = photos[1: -1]
-                    write_msg(
-                        event.user_id, f'{first_name} {last_name}\n{page}', chat_button, photos)
-                    for event in longpoll.listen():  # Новый лонг-пулл для получения реакции от пользователя
+@@ -81,31 +84,34 @@ def write_msg(user_id, message, keyboard=None, attachment=None):
                         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
                             request = event.text
                             if request == 'Нравится':
+                                push_pair_in_favorite(event.user_id, id) # Метод записи избранных в таблицу favorite
                                 # Метод записи избранных в таблицу favorite
                                 push_pair_in_favorite(event.user_id, id)
                                 break
 
+                            elif request == 'Дальше':
                             elif request == 'Дальше':  # Переход к next итерации
                                 break
 
+                            elif request == 'Стоп':
+                                 break
                             elif request == 'Стоп':  # Остановка итератора и выход из цикла for
                                 break
                         elif request == 'Стоп':
+                             break
                             break
                     if request == 'Стоп':
+                         menu_button = VkKeyboard(inline=True)
+                         menu_button.add_button('Начать поиск', VkKeyboardColor.POSITIVE)
+                         menu_button.add_button('Избранные', VkKeyboardColor.SECONDARY)
+                         write_msg(event.user_id, f'Что делаем?', menu_button)
+                         break
                         menu_button = VkKeyboard(inline=True)
                         menu_button.add_button(
                             'Начать поиск', VkKeyboardColor.POSITIVE)
@@ -105,6 +103,8 @@ for event in longpoll.listen():
                         break
             elif request == 'Нет':
                 write_msg(event.user_id, f'Что делаем?', menu_button)
+            
+            elif request == 'Избранные':
 
             elif request == 'Избранные':  # Конструкция для импорта данных из базы
                 favorite_data = get_favorite_data(event.user_id)
@@ -114,4 +114,5 @@ for event in longpoll.listen():
                     write_msg(
                         event.user_id, f'{first_name} {last_name}\n{page}', None, photos)
             else:
-                write_msg(event.user_id, "Не поняла вашего ответа...")
+                write_msg(event.user_id, "Не поняла вашего ответа...")                                
+            
